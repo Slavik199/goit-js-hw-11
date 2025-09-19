bd5e03b69e3e0de340400ea2bcf0b555597d17e3
Slavik199 <rizikovslavik@gmail.com>
2025-09-19 21:24:00 +0300

    js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { getImagesByQuery } from './js/pixabay-api';
 import { createGallery, clearGallery, showLoader, hideLoader } from './js/render-functions';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+import 'izitoast/dist/css/iziToast.min.css';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const form = document.querySelector('.form');
 
